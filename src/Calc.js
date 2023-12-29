54def18ed5bd6923aded4f691b45c57ac36f86a7
Ally-3 <146335458+Ally-3@users.noreply.github.com>
2023-12-29 17:04:36 +0000

    sound added, css changed, js code mapped

--- src/Calc.js
+++ src/Calc.js
@@ -1,9 +1,11 @@
 import "./Calc.css";
+import robotSound from './sound/robot-sound.mp4';
 import React, { useState } from 'react';
         // use of "npm install mathjs"
         //importing math object from the mathjs library
 import { create, all } from 'mathjs';
 const math = create(all);
+
 
 const Calculator = () => {
   const [display, setDisplay] = useState('');
@@ -11,14 +13,27 @@ const Calculator = () => {
         //shown in the input field.
         //setDisplay is a function used to update the value of display.
 
+  const btnData = [
+    {val: ['1', '2', '3', '+']},
+    {val: ['4', '5', '6', '-']},
+    {val: ['7', '8', '9', '*']},
+  ];
+
+  const playAudio = () => {
+   var audio = new Audio((robotSound));
+   audio.play();
+  };
+
   const handleClick = (value) => {
     setDisplay((prevDisplay) => prevDisplay + value);
+    playAudio();
   };
         //handleClick handles the click event when a button is pressed. 
         //It updates the display state by appending 
         //the clicked value to the existing value.
 
   const calculateResult = () => {
+    handleClick();
     try {
       const result = math.evaluate(display);
       setDisplay(result.toString());
@@ -33,6 +48,7 @@ const Calculator = () => {
         //If an error occurs during evaluation, the display will show "Error" instead. 
 
   const clearDisplay = () => {
+    handleClick();
     setDisplay('');
   };
         //clearDisplay clears the input field by resetting the display state 
@@ -40,35 +56,27 @@ const Calculator = () => {
 
   return (
     <div className="calc-parent">
-      <input type="text" value={display} readOnly />
-      <div>
-        <button onClick={() => handleClick('1')}>1</button>
-        <button onClick={() => handleClick('2')}>2</button>
-        <button onClick={() => handleClick('3')}>3</button>
-        <button onClick={() => handleClick('+')}>+</button>
+      <div className="calc-child">
+        <input type="text" value={display} readOnly />
       </div>
-      <div>
-        <button onClick={() => handleClick('4')}>4</button>
-        <button onClick={() => handleClick('5')}>5</button>
-        <button onClick={() => handleClick('6')}>6</button>
-        <button onClick={() => handleClick('-')}>-</button>
-      </div>
-      <div>
-        <button onClick={() => handleClick('7')}>7</button>
-        <button onClick={() => handleClick('8')}>8</button>
-        <button onClick={() => handleClick('9')}>9</button>
-        <button onClick={() => handleClick('*')}>*</button>
-      </div>
-      <div>
+        {btnData.map((btnRow, rowIndex) => (
+          <div className="calc-child" key={rowIndex}>
+            {btnRow.val.map((val, colIndex) => (
+              <button key={colIndex} onClick={() => handleClick(val)}>
+                {val}
+              </button>
+            ))}
+          </div>
+        ))}
+      <div className="calc-child">
         <button onClick={() => handleClick('0')}>0</button>
         <button onClick={() => handleClick('.')}>.</button>
         <button style={{backgroundColor: "red"}} onClick={clearDisplay}>C</button>
         <button onClick={() => handleClick('/')}>/</button>
       </div>
-      <div>
+      <div className="calc-child">
         <button style={{width: 200, fontSize: 40}} onClick={calculateResult}>=</button>
       </div>
-      <br />
     </div>
   );
 };
